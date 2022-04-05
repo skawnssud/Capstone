a7d1fd6054f75051e7a42118408a208ebaed843d
duyg5 <duyg5@naver.com>
2022-04-05 20:22:09 +0900

    Update HomeMain.kt

--- app/src/main/java/com/example/capstone/HomeMain.kt
+++ app/src/main/java/com/example/capstone/HomeMain.kt
@@ -7,19 +7,15 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.capstone.databinding.ActivityHomeMainBinding
+
 
 class HomeMain : AppCompatActivity() {
 
-    private lateinit var binding: ActivityHomeMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityHomeMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_home_main)
         // Passing each menu ID as a set of Ids because each
@@ -30,6 +26,6 @@ class HomeMain : AppCompatActivity() {
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
     }
 }
